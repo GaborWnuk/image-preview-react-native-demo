a027ed271ef54a5de7ae1f454c23c92e1004f435
Gabor Wnuk <gabor.wnuk@grupawp.pl>
2016-11-25 22:44:49 +0100

    Android bootstrap.

--- index.android.js
+++ index.android.js
@@ -1,28 +1,35 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
-'use strict';
+import React, { Component } from 'react';
+import { AppRegistry, StatusBar } from 'react-native';
+import Navigation from './app/navigation';
 
-var React = require('react-native');
-var Main = require('./app/main');
+import { Router } from './app/routing'
 
-var {
-  AppRegistry,
-  Component,
-  View
-} = React;
+import {
+  NavigationProvider,
+} from '@exponent/ex-navigation';
 
-
-class ImagePreviewReactNativeDemo extends Component {
-
+class App extends Component {
   render() {
-
-      return (
-        <Main/>
-      )
+    /**
+      * NavigationProvider is only needed at the top level of the app,
+      * similar to react-redux's Provider component. It passes down
+      * navigation objects and functions through context to children.
+      *
+      * StackNavigation represents a single stack of screens, you can
+      * think of a stack like a stack of playing cards, and each time
+      * you add a screen it slides in on top. Stacks can contain
+      * other stacks, for example if you have a tab bar, each of the
+      * tabs has its own individual stack. This is where the playing
+      * card analogy falls apart, but it's still useful when thinking
+      * of individual stacks.
+      */
+    return (
+      <NavigationProvider router={Router}>
+        <StatusBar barStyle="light-content" />
+        <Navigation />
+      </NavigationProvider>
+    );
   }
 }
 
-
-AppRegistry.registerComponent('ImagePreviewReactNativeDemo', () => ImagePreviewReactNativeDemo);
+AppRegistry.registerComponent('App', () => App);
