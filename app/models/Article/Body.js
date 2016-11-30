ddf1ac77955152cd14026aa41b07149c5565a187
Gabor Wnuk <gabor.wnuk@grupawp.pl>
2016-11-30 12:11:01 +0100

    Basic implementation of Realm.

---
+++ app/models/Article/Body.js
@@ -0,0 +1,14 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+export default class Body {}
+
+Body.schema = {
+    name: 'Body',
+    properties: {
+        data: {type: 'string'},
+    },
+};
---
+++ app/models/Article/index.js
@@ -0,0 +1,22 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+export default class Article {
+  static get(offset) {
+
+    return ":)"
+  }
+}
+
+Article.schema = {
+    name: 'Article',
+    properties: {
+        id: {type: 'string'},
+        title: {type: 'string'},
+        ts: {type: 'int'},
+        body: {type: 'list', objectType: 'Body'}
+    },
+};
