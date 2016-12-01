c49b1e07b805815c7e8a6688503b0dc7aceee3c5
Gabor Wnuk <gabor.wnuk@grupawp.pl>
2016-12-01 09:16:24 +0100

    Add missing Img module.

---
+++ app/models/Article/Img.js
@@ -0,0 +1,15 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+export default class Img {}
+
+Img.schema = {
+    name: 'Img',
+    properties: {
+        url: {type: 'string'},
+        b64: {type: 'string'}
+    },
+};
